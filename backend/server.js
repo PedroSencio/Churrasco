2cb0661b7d7f1b4ef1a8d5c4b3862810738b8320
Pedro Henrique Sencio <pedrosencio@MacBook-Air-de-Pedro.local>
2025-06-18 19:42:33 -0300

    sla

--- backend/server.js
+++ backend/server.js
@@ -64,9 +64,7 @@ app.post('/gerar-pix', async (req, res) => {
     if (!req.body.cpf || !/^\d{11}$/.test(req.body.cpf.replace(/\D/g, ''))) {
       return res.status(400).json({ error: "CPF inválido" });
     }
-    if (req.body.valor < 5) {
-      return res.status(400).json({ error: "Valor mínimo: R$5,00" });
-    }
+    
 
     const pagamento = await mercadopago.payment.create({
       transaction_amount: req.body.valor,
